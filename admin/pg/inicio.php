b3e6a4f9e3b6082f82f9f4dbe1e5549138e72015
Anderson Donaire <andersondonaire@hotmail.com>
2024-10-18 09:30:14 -0300

    1810-0930

--- admin/pg/inicio.php
+++ admin/pg/inicio.php
@@ -1,1 +1,13 @@
-<h1>Selecione o menu acima</h1>
+<h1>Home</h1>
+
+<form class="form-group" method="post" enctype="multipart/form-data">
+<label for="nome_perfil">Nome de Perfil</label>    
+<input class="form-control" type="text" name="nome_perfil" id="nome_perfil">
+<label for="link_face">Link do Facebook</label>
+<input class="form-control" type="text" name="link_face" id="link_face">
+<label for="link_insta">Link do Instagram</label>
+<input class="form-control" type="text" name="link_insta" id="link_insta">
+<label for="link_linkedin">Link do Linkedin</label>
+<input class="form-control" type="text" name="link_linkedin" id="link_linkedin">
+
+</form>
