a842ae63267b86b2115613193d17b9cbbeed7ae6
Anderson Donaire <andersondonaire@hotmail.com>
2024-11-05 08:51:31 -0300

    07414

--- admin/pg/inicio.php
+++ admin/pg/inicio.php
@@ -11,25 +11,28 @@ if (isset($_POST['salvar'])) {
     $txt_home = explode(",", $txt_home);
 
 
-    var_dump($txt_home);
+    // var_dump($txt_home);
 
     $json_txt_home = json_encode($txt_home, true);
 
     Helpers::setSettings("txt_home", $json_txt_home);
 
+    
     //IMAGENS
+
+    $caminho = "../img/";
 
     if (isset($_FILES['img_profile'])) {
 
         $img = $_FILES['img_profile'];
 
-        if (file_exists(__DIR__ . "../../img/" . Helpers::getSettings('img_profile'))) {
-            unlink(__DIR__ . "../../img/" . Helpers::getSettings('img_profile'));
+        if (file_exists($caminho . Helpers::getSettings('img_profile'))) {
+            unlink($caminho . Helpers::getSettings('img_profile'));
         }
 
-        $caminho = '../../img/';
+       
 
-        include_once "./admin/class/upload/class.upload.php";
+        include_once "./class/upload/class.upload.php";
 
         $imgPerfil = new \Verot\Upload\Upload($_FILES['img_profile']);
 
@@ -44,12 +47,43 @@ if (isset($_POST['salvar'])) {
             if ($imgPerfil->processed) {
                 $imgPerfil->clean();
 
-                $rImgPerfil = $sql->update("settings", ['setting_value' => $imgPerfil->file_dst_name], "setting_key=img_profile");
-               
+                $rImgPerfil = connect::update("settings", ['setting_value' => $imgPerfil->file_dst_name], "setting_key='img_profile'");
+
                 if ($rImgPerfil['codErro'] != 0) {
+                    echo "Erro ao salvar a imagem do perfil no banco de dados.{$rImgPerfil['msg']}";
+                }
+            } else {
+                echo 'Erro ao processar a imagem do perfil: ' . $imgPerfil->error;
+            }
+        }
+    }
+
+
+    // Processamento da imagem de fundo
+    if (!empty($_FILES['img_fundo']['name'])) {
+
+        //buscar nome da imagem no banco de dados
+        $img_antiga = Helpers::getSettings("img_fundo");
+
+        if (file_exists($caminho . $img_antiga)) {
+            unlink($caminho . $img_antiga);
+        }
+
+        $imgFundo = new \Verot\Upload\Upload($_FILES['img_fundo']);
+
+        if ($imgFundo->uploaded) {
+            $imgFundo->file_new_name_body = 'img_fundo';
+            $imgFundo->image_resize = true;
+            $imgFundo->image_convert = 'webp';
+            $imgFundo->image_x = 1920; // tamanho maior para a imagem de fundo
+            $imgFundo->image_ratio_y = true;
+            $imgFundo->process($caminho);
+            if ($imgFundo->processed) {
+                $imgFundo->clean();
+                $rImgFundo = $sql->update("settings", ['setting_value' => $imgFundo->file_dst_name], "id=7");
+                if ($rImgFundo['codErro'] != 0) {
                     echo "Erro ao salvar a imagem do perfil no banco de dados.";
                 }
-
             } else {
                 echo 'Erro ao processar a imagem do perfil: ' . $imgPerfil->error;
             }
@@ -100,7 +134,7 @@ if (isset($_POST['salvar'])) {
     <br>
 
     <label for="img_background">Imagem de Fundo</label><br>
-    <input type="file" name="img_background" id="img_background" accept="image/*">
+    <input type="file" name="img_background" id="img_fundo" accept="image/*">
 
     <br>
     <input class="btn btn-success float-end" type="submit" value="Salvar" name="salvar">
